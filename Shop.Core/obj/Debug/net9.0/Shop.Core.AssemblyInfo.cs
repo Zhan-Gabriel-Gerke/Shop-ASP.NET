51c0acf793d7cbc9f5f3fb506e4d466ceef8c887
Zhan-Gabriel Gerke <zangerke3@gmail.com>
2025-09-16 18:02:39 +0300

    save

--- Shop.Core/obj/Debug/net9.0/Shop.Core.AssemblyInfo.cs
+++ Shop.Core/obj/Debug/net9.0/Shop.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shop.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63e723b79a400887c34d251ed4bce50f858de6c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+962e8e5e1a12daf1ca44b650f828e3bb30b52d47")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shop.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shop.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
